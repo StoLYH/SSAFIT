72fff3af4ffa988c54d342475421df2485e44717
yonghoon <yy3082@dgu.ac.kr>
2025-05-02 15:17:00 +0900

    update2

---
+++ wangyong/src/main/java/com/ssafy/mvc/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.ssafy.mvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+
+	// service DI
+	
+	
+	
+}
