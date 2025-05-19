05131aa3822506e60140c89d8dff0337dc0954c6
yonghoon <yy3082@dgu.ac.kr>
2025-05-19 19:22:56 +0900

    up

--- wangyong/src/main/java/com/ssafy/mvc/exception/BoardException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.mvc.exception;
-
-public class BoardException extends RuntimeException { 
-	public BoardException() {}
-
-	public BoardException(String message) {		
-		super(message);			// 메세지 사용하면서 예외 터트리기 -> e.getmessage를 사용하기
-	}
-}
